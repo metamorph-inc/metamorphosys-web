d5c6c79b1325176a7b2c0f8c967539fc6734f30e
James Klingler <jklingler@isis.vanderbilt.edu>
2014-07-29 10:35:51 -0500

    call the update libraries script before launching

--- launch_app.cmd
+++ launch_app.cmd
@@ -1,7 +1,13 @@
 echo off
 where node
 if %ERRORLEVEL% == 0 (
-	node app.js
+    update_libraries.cmd
+    if %ERRORLEVEL% == 0 (
+        node app.js
+    ) else (
+        echo ERROR: Failed to update libraries!
+        pause
+    )
 ) else (
 	echo ERROR: node is not in path! Make sure to install node and append it to Path.
 	pause
