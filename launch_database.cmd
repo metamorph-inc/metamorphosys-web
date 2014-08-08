737d00ed2074b64e3dbc8bfcb3a0f0c74e5582b0
Kevin Smyth <kevin.m.smyth@gmail.com>
2014-08-08 13:32:49 -0500

    Dont exit my cmd

--- launch_database.cmd
+++ launch_database.cmd
@@ -1,7 +1,7 @@
 echo off
 if exist "C:\Program Files\MongoDB 2.6 Standard\bin" (
     "C:\Program Files\MongoDB 2.6 Standard\bin\mongod" --dbpath mongodatabase
-exit
+    exit /b %ERRORLEVEL%
 ) else (
     echo ERROR: "C:\Program Files\MongoDB 2.6 Standard\bin\mongod" does not exist.
     echo Make sure to install mongodb. If it is already installed at different location
