792574a3263fe80c100dc4321294d2e696e6dc18
pmeijer <patrik85@isis.vanderbilt.edu>
2014-09-08 19:28:35 -0500

    First attempt of getting latest commit when fast forwarding isn't possible.

--- src/plugins/ADMEditor/TestBenchRunner/TestBenchRunner.js
+++ src/plugins/ADMEditor/TestBenchRunner/TestBenchRunner.js
@@ -810,5 +810,89 @@ define(['plugin/PluginConfig',
         return (lastIndex !== -1) && (lastIndex + ending.length === str.length);
     };
 
+    TestBenchRunner.prototype.save2 = function (message, callback) {
+        var self = this;
+
+        this.logger.debug('Saving project');
+
+        // Commit changes.
+        this.core.persist(this.rootNode, function (err) {
+            // TODO: any error here?
+            if (err) {
+                self.logger.error(err);
+            }
+        });
+
+        var newRootHash = this.core.getHash(this.rootNode);
+
+        var commitMessage = '[Plugin] ' + this.getName() + ' (v' + this.getVersion() + ') updated the model.';
+        if (message) {
+            commitMessage += ' - ' + message;
+        }
+
+        this.currentHash = this.project.makeCommit([this.currentHash], newRootHash, commitMessage, function (err) {
+            // TODO: any error handling here?
+            if (err) {
+                self.logger.error(err);
+            }
+        });
+
+        if (this.branchName) {
+            // try to fast forward branch if there was a branch name defined
+
+            // FIXME: what if master branch is already in a different state?
+
+            this.project.getBranchNames(function (err, branchNames) {
+                if (branchNames.hasOwnProperty(self.branchName)) {
+                    var branchHash = branchNames[self.branchName];
+                    if (branchHash === self.branchHash) {
+                        // the branch does not have any new commits
+                        // try to fast forward branch to the current commit
+                        self.project.setBranchHash(self.branchName, self.branchHash, self.currentHash, function (err) {
+                            if (err) {
+                                // fast forward failed
+                                self.logger.error(err);
+                                self.logger.info('"' + self.branchName + '" was NOT updated');
+                                self.logger.info('Project was saved to ' + self.currentHash + ' commit.');
+                            } else {
+                                // successful fast forward of branch to the new commit
+                                self.logger.info('"' + self.branchName + '" was updated to the new commit.');
+                                // roll starting point on success
+                                self.branchHash = self.currentHash;
+                            }
+                            callback(err);
+                        });
+                    } else {
+                        // branch has changes a merge is required
+                        // TODO: try auto-merge, if fails ...
+                        self.logger.warn('Cannot fast forward "' + self.branchName + '" branch. Merge is required but not supported yet.');
+                        self.logger.info('Project was saved to ' + self.currentHash + ' commit.');
+                        self.logger.info('Loading latest commit, from ' + self.branchHash);
+                        self.project.getBranchHash(self.branchName, self.branchHash, function (err, arg1, arg2) {
+                            if (err) {
+                                self.logger.error(err);
+                                return;
+                            }
+                            console.log('arg1', arg1);
+                            console.log('arg1', arg2);
+                            callback(null);
+                        });
+                    }
+                } else {
+                    // branch was deleted or not found, do nothing
+                    self.logger.info('Project was saved to ' + self.currentHash + ' commit.');
+                    callback(null);
+                }
+            });
+            // FIXME: is this call async??
+            // FIXME: we are not tracking all commits that we make
+
+        } else {
+            // making commits, we have not started from a branch
+            this.logger.info('Project was saved to ' + this.currentHash + ' commit.');
+            callback(null);
+        }
+    };
+
     return TestBenchRunner;
 });
