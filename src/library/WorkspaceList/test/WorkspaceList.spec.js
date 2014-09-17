db29b92d104c59dd3b8c1eeb62ad984589e81788
Zsolt Lattmann <lattmann@isis.vanderbilt.edu>
2014-09-16 20:57:07 -0500

    Add a sample spec file, tests are not configured yet.

---
+++ src/library/WorkspaceList/test/WorkspaceList.spec.js
@@ -0,0 +1,16 @@
+'use strict';
+
+describe('webgme-cyphy.workspaceList module', function () {
+
+    beforeEach(module('webgme-cyphy.workspaceList'));
+
+    describe('workspaceList controller', function () {
+
+        it('should exist', inject(function ($controller) {
+            //spec body
+            var workspaceListController = $controller('WorkspaceListController');
+            expect(workspaceListController).toBeDefined();
+        }));
+
+    });
+});
