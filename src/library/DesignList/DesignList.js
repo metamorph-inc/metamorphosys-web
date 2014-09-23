183449572ad9575ef0112893b7b5134d4603819e
pmeijer <patrik85@isis.vanderbilt.edu>
2014-09-22 19:16:15 -0500

    Example to show the interfaces. Functions should probably be in a DesertConfiguration(List) instead.

--- src/library/DesignList/DesignList.js
+++ src/library/DesignList/DesignList.js
@@ -7,9 +7,11 @@
 
 require('../WorkspaceList/WorkspaceList');
 
-angular.module('cyphy.designs')
-    .service('DesignService', function () {
+angular.module('cyphy.designs', ['gme.services'])
+    .service('DesignService', function ($q, NodeService) {
         'use strict';
+        var watchers = {};
+
         this.deleteDesign = function (designId) {
             throw new Error('Not implemented yet.');
         };
@@ -56,6 +58,8 @@ angular.module('cyphy.designs')
             throw new Error('Not implemented yet.');
         };
 
+        // FIXME: watchConfigurationSets and watchConfigurations should probably go to a DesertConfiguration-Service,
+        // with a related controller DesertConfigurationSetList, where details are configurations.
         /**
          *  Watches the generated DesertConfigurationSets inside a Design.
          * @param parentContext - context of controller.
@@ -63,17 +67,123 @@ angular.module('cyphy.designs')
          * @param updateListener - invoked when there are (filtered) changes in data.
          */
         this.watchConfigurationSets = function (parentContext, designId, updateListener) {
-            throw new Error('Not implemented yet.');
+            var deferred = $q.defer(),
+                data = {
+                    name: null,
+                    id: designId,
+                    regionId: null,
+                    cfgSets: {}
+                },
+                context = {
+                    db: parentContext.db,
+                    projectId: parentContext.projectId,
+                    branchId: parentContext.branchId,
+                    regionId: parentContext.regionId + '_watchConfigurationSets_' + designId
+                };
+            data.regionId = context.regionId;
+            watchers[parentContext.regionId] = watchers[parentContext.regionId] || {};
+            watchers[parentContext.regionId][context.regionId] = context;
+            console.log('Added new watcher: ', watchers);
+            NodeService.logContext(context);
+            NodeService.getMetaNodes(context).then(function (meta) {
+                NodeService.loadNode(context, designId)
+                    .then(function (designNode) {
+                        data.name = designNode.getAttribute('name');
+                        designNode.loadChildren(context)
+                            .then(function (childNodes) {
+                                var i,
+                                    onUpdate = function (id) {
+                                        var newName = this.getAttribute('name'),
+                                            newDesc = this.getAttribute('INFO');
+                                        console.warn(newName);
+                                        if (newName !== data.cfgSets[id].name ||
+                                                newDesc !== data.cfgSets[id].description) {
+                                            //data.cfgSets[id].name = newName;
+                                            //console.warn('changed');
+                                            updateListener(true);
+                                        }
+                                    },
+                                    onUnload = function (id) {
+                                        updateListener(true);
+                                    };
+                                for (i = 0; i < childNodes.length; i += 1) {
+                                    if (childNodes[i].isMetaTypeOf(meta.DesertConfigurationSet)) {
+                                        data.cfgSets[childNodes[i].getId()] = {
+                                            id: childNodes[i].getId(),
+                                            name: childNodes[i].getAttribute('name'),
+                                            description: childNodes[i].getAttribute('INFO')
+                                        };
+                                        childNodes[i].onUpdate(onUpdate);
+                                        childNodes[i].onUnload(onUnload);
+                                    }
+                                }
+                                //console.log('cfgSets', cfgSets);
+                                designNode.onNewChildLoaded(function (newNode) {
+                                    if (newNode.isMetaTypeOf(meta.DesertConfigurationSet)) {
+                                        updateListener(true);
+                                    }
+                                });
+                                deferred.resolve(data);
+                            });
+                        designNode.onUpdate(function (id) {
+                            var newName = this.getAttribute('name');
+                            if (newName !== data.name) {
+                                data.name = newName;
+                                updateListener(true);
+                            }
+                        });
+                    });
+            });
+            return deferred.promise;
+        };
+
+        // FIXME: Watching configurations inside a configuration-set does probably not belong here.
+//        /**
+//         *  Watches the DesertConfiguration (w.r.t. existence and attributes) inside a DesertConfigurationSet.
+//         * @param parentContext - context of controller.
+//         * @param configurationSetId
+//         * @param updateListener - invoked when there are (filtered) changes in data.
+//         */
+//        this.watchConfigurations = function (parentContext, configurationSetId, updateListener) {
+//            throw new Error('Not implemented yet.');
+//        };
+
+        /**
+         * Removes all watchers spawned from parentContext.
+         * @param parentContext - context of controller.
+         */
+        this.cleanUpAllRegions = function (parentContext) {
+            var childWatchers,
+                key;
+            if (watchers[parentContext.regionId]) {
+                childWatchers = watchers[parentContext.regionId];
+                for (key in childWatchers) {
+                    if (childWatchers.hasOwnProperty(key)) {
+                        NodeService.cleanUpRegion(childWatchers[key]);
+                    }
+                }
+                delete watchers[parentContext.regionId];
+            } else {
+                console.log('Nothing to clean-up..');
+            }
         };
 
         /**
-         *  Watches the DesertConfiguration (w.r.t. existence and attributes) inside a DesertConfigurationSet.
-         * @param parentContext - context of controller.
-         * @param configurationSetId
-         * @param updateListener - invoked when there are (filtered) changes in data.
+         * Removes specified watcher (regionId)
+         * @param parentContext
+         * @param regionId
          */
-        this.watchConfigurations = function (parentContext, configurationSetId, updateListener) {
-            throw new Error('Not implemented yet.');
+        this.cleanUpRegion = function (parentContext, regionId) {
+            if (watchers[parentContext.regionId]) {
+                if (watchers[parentContext.regionId][regionId]) {
+                    NodeService.cleanUpRegion(regionId);
+                    delete watchers[parentContext.regionId][regionId];
+                } else {
+                    console.log('Nothing to clean-up..');
+                }
+            } else {
+                console.log('Cannot clean-up region since parentContext is not registered..', parentContext);
+            }
         };
     })
     .controller('DesignListController', function ($scope, DesignService) {
