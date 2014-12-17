db0b8889a427b6d9ef1f32011f9831988bd248f3
Kevin Smyth <kevin.m.smyth@gmail.com>
2014-12-17 07:33:22 -0600

    Note bower install

--- build.cmd
+++ build.cmd
@@ -1,6 +1,7 @@
 SetLocal EnableDelayedExpansion
 
 rem npm install || exit /b !ERRORLEVEL!
+rem bower install || exit /b !ERRORLEVEL!
 node src\plugins\ADMEditor\TestBenchRunner\Templates\combine_templates.js || exit /b !ERRORLEVEL!
 node test\models\combine_templates.js || exit /b !ERRORLEVEL!
 node ./node_modules/requirejs/bin/r.js -o ./utils/build/webcyphy.plugins/cbuild.js || exit /b !ERRORLEVEL!
