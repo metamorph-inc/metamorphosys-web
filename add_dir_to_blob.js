4f724866091104299f89a24116436a1d0522d4f0
Kevin Smyth <kevin.m.smyth@gmail.com>
2015-01-23 11:15:19 -0600

    Change add_dir_to_blob.js to use http, be able to upload one file

--- add_dir_to_blob.js
+++ add_dir_to_blob.js
@@ -13,8 +13,6 @@ if (process.argv.length < 3) {
     return;
 }
 
-var requirejs = require('requirejs');
-
 var config = require('./config.json'),
     webgme = require('webgme'),
     fs = require('fs'),
@@ -22,15 +20,25 @@ var config = require('./config.json'),
 
 var startDir = path.resolve(process.argv[2]);
 
+config.paths.blob = './node_modules/webgme/src/middleware/blob'
 // updating default configuration with ours
 WebGMEGlobal.setConfig(config);
 
-var BlobFSBackend = requirejs('blob/BlobFSBackend');
-var BlobRunPluginClient = requirejs('blob/BlobRunPluginClient');
+//var requirejs = WebGMEGlobal.requirejs;
+//var BlobFSBackend = requirejs('blob/BlobFSBackend');
+//var BlobRunPluginClient = requirejs('blob/BlobRunPluginClient');
+//var blobClient = new BlobRunPluginClient(blobBackend);
 
-// FIXME: should we use HTTP here?
-var blobBackend = new BlobFSBackend();
-var blobClient = new BlobRunPluginClient(blobBackend);
+var bc = WebGMEGlobal.requirejs('blob/BlobClient');
+var host, port, httpsecure;
+if (process.argv[3]) {
+    var url = require('url').parse(process.argv[3]);
+} else {
+    host = 'localhost';
+    port = config.port;
+    httpsecure = false;
+}
+var blobClient = new bc({server: process.argv[3] || , serverPort: port, httpsecure: httpsecure });
 
 var walk = function(dir, done) {
     var results = [];
@@ -57,11 +65,20 @@ var walk = function(dir, done) {
 };
 
 
-walk(startDir, function (err, files) {
+if (fs.statSync(startDir).isDirectory()) {
+    walk(startDir, AddFiles);
+} else {
+    var files = [startDir];
+    startDir = path.resolve(startDir, '..');
+    AddFiles(null, files);
+}
+
+function AddFiles(err, files, callback) {
     if (err) {
         console.error(err);
         return;
     }
+    callback = callback || function() {};
 
     console.log('Directory ' + startDir + ' has ' + files.length + ' files.');
 
@@ -83,11 +100,13 @@ walk(startDir, function (err, files) {
 
         var filename = path.relative(startDir, files[i]).replace(/\\/g,'/');
 
-        resultArtifact.addFileAsSoftLink(filename, fs.createReadStream(files[i]), function (err, hash) {
+        // TODO: stream the file
+        resultArtifact.addFileAsSoftLink(filename, fs.readFileSync(files[i]), function (err, hash) {
             remaining -= 1;
 
             if (err) {
-                console.error(err);
+                console.error("Adding file failed: " + err);
+                callback(err);
                 return;
             }
 
@@ -95,6 +114,7 @@ walk(startDir, function (err, files) {
                 resultArtifact.save(function (err, artifactHash) {
                     if (err) {
                         console.error(err);
+                        callback(err);
                         return;
                     }
 
@@ -105,10 +125,13 @@ walk(startDir, function (err, files) {
                     console.log(artifactName + ' - localhost:' + port);
                     console.log(' - metadata: localhost:' + port + blobClient.getMetadataURL(artifactHash));
                     console.log(' - download: localhost:' + port + blobClient.getDownloadURL(artifactHash));
+                    callback(null, artifactHash);
 
                 });
             }
         });
     }
 
-});
+}
+
+module.exports.AddFiles = AddFiles;
