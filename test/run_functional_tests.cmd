5fae43ad2793d46885500756f101fd1ca6c8234d
Kevin Smyth <kevin.m.smyth@gmail.com>
2015-01-28 16:05:14 -0600

    Run tests against mongo db 'CyPhyFunctional' and drop it before runs to ensure a clean slate

--- test/run_functional_tests.cmd
+++ test/run_functional_tests.cmd
@@ -8,6 +8,9 @@ where forever || exit /b !ERRORLEVEL!
 
 del cyphy_functional_server_*.log
 
+set PATH=%PATH%;c:\Program Files\MongoDB 2.6 Standard\bin
+mongo CyPhyFunctional --eval "printjson(db.dropDatabase())"
+
 cmd /c forever stop cyphy_functional_server
 
 cmd /c forever start --uid cyphy_functional_server -l %CD%\cyphy_functional_server_forever.log -o cyphy_functional_server_1.log -e cyphy_functional_server_2.log  app.js || exit /b !ERRORLEVEL!
