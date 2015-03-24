31db1ee569c9387a17db038c788077c1555f2a43
Robert Boyles <rboyles@metamorphsoftware.com>
2015-03-24 16:18:01 -0500

    WEB-60 minor changes

--- src/app/mmsApp/services/wiringService/classes/OrthogonalRouter.js
+++ src/app/mmsApp/services/wiringService/classes/OrthogonalRouter.js
@@ -6,13 +6,16 @@ var OrthogonalRouter = function () {
 
     var self = this;
 
-    this.name = 'OrthogonalRouter';
+    self.name = 'OrthogonalRouter';
 
-    this.visibilityGraph = [];
+    this.visibilityGraph = {
+        vertices: [],
+        edges: []
+    };
 
     this.connections = [];
 
-    this.routeConnections = function ( components, connections ) {
+    this.routeConnections = function ( components, connections, grid ) {
         // Step 1: generateVisibilityGraph
         // Step 2: AutoRouteWithGraph
         // Step 3: nudgeRoutes
@@ -20,7 +23,7 @@ var OrthogonalRouter = function () {
         // Step 5: drawConnections <-- Is this done here or somewhere else??
     };
 
-    this.generateVisibilityGraph = function ( components ) {
+    this.generateVisibilityGraph = function ( components, grid ) {
         /*
             Takes in the set of components with their coordinates of interest and generates orthogonal visibility grid.
 
@@ -36,18 +39,118 @@ var OrthogonalRouter = function () {
                 ports: list<port>
             }
 
-             var port = {
-                 name: <String>,
-                 X: <double>,
-                 Y: <double>
-             }
+            var port = {
+                name: <String>,
+                X: <double>,
+                Y: <double>,
+                direction: <String> ("N", "S", "E", W")
+            }
+
+            grid: Size of style sheet for boundary calculations, and square increments. Eg,
+            var grid = {
+                length: <double>,
+                height: <double>,
+                increment: <double>
+            }
+
          */
-        var visibilityGraph = [];
-        this.visibilityGraph = visibilityGraph;
+        var edges = [];
+
+        // Get interesting nodes from components.
+        this.visibilityGraph.vertices = this.getNodes( components );
+
+        // We will first get the interesting horizontal line segment, so sort by Y coordinate
+        var sortedY = this.visibilityGraph.vertices;
+        sortedY.sort(function ( a, b ) { return self.compare(a, b, 0); });
+
+        var horzSegments = this.lineSweep(sortedY, "horizontal", grid.width);
+
+
+
+
+        this.visibilityGraph.edges = edges;
     };
 
-    this.lineSweep = function ( components, method ) {
+    this.getNodes = function ( components ) {
+        var i;
+        for (i = 0; i < components.length; i++) {
+            this.getNodesFromComponent(components[i]);
+        }
+        return this.visibilityGraph.vertices;
+    };
+
+    this.getNodesFromComponent = function ( component ) {
+        this.visibilityGraph.vertices.push( { x: component.boundingBox.xMin, y: component.boundingBox.yMin } );
+        this.visibilityGraph.vertices.push( { x: component.boundingBox.xMin, y: component.boundingBox.yMax } );
+        this.visibilityGraph.vertices.push( { x: component.boundingBox.xMax, y: component.boundingBox.yMin } );
+        this.visibilityGraph.vertices.push( { x: component.boundingBox.xMax, y: component.boundingBox.yMax } );
+
+        var i;
+        for ( i = 0; i < component.ports.length; i++ ) {
+            this.visibilityGraph.vertices.push({x: component.ports[i].x,
+                                                y: component.ports[i].y,
+                                                isPort: true,
+                                                direction: component.ports[i].direction});
+        }
+    };
+
+    this.lineSweep = function ( nodes, method, sweepLength ) {
       // Perform line sweep algorithm on grid. Method can either be vertical or horizontal implementation of line sweep.
+        var segments = [],
+            openObjects = [],
+            i;
+
+        // Components are sorted by Y coordinate and then X coordinate for position.
+        // The Y coordinate sorting allows for the event queue to be know (steps in for loop where sweep line will go).
+
+        // Due to sorting, each node's closest left and right neighbors are known.
+        // If first or last entry, neighbor is boundary of grid.
+
+        // If neighbor is at the same Y coordinate, objects are colinear, so grab the nearest neighbor of the neighbor.
+        // Repeat until either neighbor is not colinear or boundary is hit.
+
+        // At each event an object will be either open or closed. If object is opened, compare coordinates as above,
+        // but also check the coordinates of any nodes in the openObjects queue.
+
+        for ( i = 0; i < nodes.length; i++ ) {
+            // If openObjects does not have an entry at X coords, then new object is encountered. Add to openObject.
+            var sLeft = nodes[i],
+                sRight = nodes[i+1];
+
+            if ( openObjects.length > 0 ) {
+                // Some objects are open, work here...
+            }
+            else {
+                openObjects.push({ sLeft: sLeft, sRight: sRight });
+
+
+            }
+
+            //  
+
+        }
+
+
+
+        return segments;
+    };
+
+    this.compare = function ( node, compareNode, order ) {
+        if ( order === 1 ) {
+            // Sort by X then Y (Vertical line sweep)
+            if ( node.x > compareNode.x ) return 1;
+            if ( node.x < compareNode.x ) return -1;
+            if (node.y > compareNode.y ) return 1;
+            if (node.y < compareNode.y ) return -1;
+        }
+        else {
+            // Sort by Y then X (Horizontal line sweep)
+            if (node.y > compareNode.y ) return 1;
+            if (node.y < compareNode.y ) return -1;
+            if ( node.x > compareNode.x ) return 1;
+            if ( node.x < compareNode.x ) return -1;
+        }
+        return 0; // Same point
     };
 
     this.AutoRouteWithGraph = function ( visibilityGrid, connections ) {
@@ -94,4 +197,4 @@ var OrthogonalRouter = function () {
 
 };
 
-module.exports = OrthogonalRouter;
+//module.exports = OrthogonalRouter;
