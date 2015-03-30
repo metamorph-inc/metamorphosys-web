222dbfedf8f87d79316b6408ad0f5d3d60360474
pmeijer <patrik85@isis.vanderbilt.edu>
2015-03-30 10:59:48 -0500

    Added missed config files.

---
+++ config/config.default.js
@@ -0,0 +1,33 @@
+/*jshint node: true*/
+/**
+ * @author lattmann / https://github.com/lattmann
+ */
+
+var config = require('webgme/config/config.default');
+
+config.server.port = 8855;
+config.mongo.uri = 'mongodb://127.0.0.1:27017/CyPhy';
+
+config.plugin.basePaths.push('./src/plugins/ADMEditor');
+config.plugin.allowServerExecution = true;
+
+config.executor.enable = true;
+
+config.visualization.visualizerDescriptors.push('./Visualizers.json');
+
+//config.rest.components.desert = './src/rest/desert/Desert';
+//config.rest.components.serverinfo = './src/rest/serverInfo/ServerInfo';
+
+config.requirejsPaths.ejs = './node_modules/webgme/src/common/util/ejs';
+config.requirejsPaths.xmljsonconverter = './utils/xmljsonconverter';
+config.requirejsPaths.sax = './vendor/sax/sax';
+config.requirejsPaths.executor = './node_modules/webgme/src/middleware/executor';
+config.requirejsPaths.requirejs = './src/client';
+config.requirejsPaths.CyPhyMETA = './meta';
+config.requirejsPaths.cyphyDist = './dist';
+
+config.requirejsPaths['panels/ProjectAnalyzer'] = './src/panels/ProjectAnalyzer';
+config.requirejsPaths['widgets/ProjectAnalyzer'] = './src/widgets/ProjectAnalyzer';
+
+
+module.exports = config;
