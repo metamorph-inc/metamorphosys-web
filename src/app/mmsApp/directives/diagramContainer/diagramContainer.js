12bd5e8396e5c3ab4cb9733b283a0f5dde84b4fe
Laszlo Juracz <laszlo.juracz@gmail.com>
2015-04-01 15:36:15 -0500

    pre-demo

--- src/app/mmsApp/directives/diagramContainer/diagramContainer.js
+++ src/app/mmsApp/directives/diagramContainer/diagramContainer.js
@@ -307,7 +307,7 @@ angular.module('mms.diagramContainer', [
                             designEditorCtrl.removeEventListener('resize', ctrl.scrollHandler.onWindowResize);
                         }
 
-                        $windowElement.unBind('resize', ctrl.scrollHandler.onWindowResize);
+                        $windowElement.unbind('resize', ctrl.scrollHandler.onWindowResize);
 
                         element.off();
                     });
