metamorph-inc/metamorphosys-web

commit 5f59a8bbc74c6aa45f0ce35b59656b10a8e02907

Update requirejs for webgme changes

utils/build/webcyphy.plugins/cbuild.js
@@ -4,14 +4,8 @@
     baseUrl: '../../../',
     paths: {
         'webcyphy.plugins': './utils/build/webcyphy.plugins/webcyphy.plugins',
-        util: './node_modules/webgme/src/common/util',
-        eventDispatcher: './node_modules/webgme/src/common/EventDispatcher',
-        core: './node_modules/webgme/src/common/core',
-        coreclient: './node_modules/webgme/src/common/core/users',
-        storage: './node_modules/webgme/src/common/storage',
-        logManager: './node_modules/webgme/src/common/LogManager',
+        common: './node_modules/webgme/src/common',
         blob: './node_modules/webgme/src/middleware/blob',
-        client: './node_modules/webgme/src/client/js/client',
         plugin: './node_modules/webgme/src/plugin',
         xmljsonconverter: './utils/xmljsonconverter',
         sax: './vendor/sax/sax',

src/panels/ProjectAnalyzer/ProjectAnalyzerPanelControl.js
@@ -1,7 +1,7 @@
 "use strict";
 
-define( [ 'logManager',
-    'clientUtil',
+define( [ 'common/LogManager',
+    'js/util',
     'js/Utils/METAAspectHelper',
     'js/Constants',
     'js/Utils/GMEConcepts',

src/plugins/ADMEditor/ProjectImporter/ProjectImporter.js
@@ -4,7 +4,7 @@ define(['plugin/PluginConfig',
     'plugin/PluginBase',
     'plugin/AdmImporter/AdmImporter/meta',
     'blob/BlobClient',
-    'logManager',
+    'common/LogManager',
     'jszip',
     'xmljsonconverter',
     '/extlib/node_modules/q/q.js',

src/rest/copyproject/CopyProject.js
@@ -3,7 +3,7 @@
  config.json: "rextrast": { "copyproject": "./src/rest/copyproject/CopyProject", ...
  http://localhost:8855/rest/external/copyproject
  */
-define(['logManager'
+define(['common/LogManager'
 ], function (logManager) {
     'use strict';
 

src/rest/desert/Desert.js
@@ -2,7 +2,7 @@
  * Created by pmeijer on 6/23/2014.
  */
 
-define( [ 'logManager',
+define( [ 'common/LogManager',
     'blob/BlobRunPluginClient',
     'blob/BlobFSBackend',
     'fs',

src/rest/serverInfo/ServerInfo.js
@@ -4,7 +4,7 @@
  */
 
 var os = require( 'os' );
-define( [ 'logManager',
+define( [ 'common/LogManager',
     'child_process'
 ], function ( logManager, child_process ) {
     'use strict';

src/widgets/ProjectAnalyzer/ProjectAnalyzerWidget.js
@@ -1,5 +1,5 @@
 define( [
-    'logManager',
+    'common/LogManager',
     'css!./styles/ProjectAnalyzerWidget.css'
 ], function ( logManager ) {
 

test-conf.js
@@ -37,7 +37,7 @@ if (testPaths) {
         paths:paths
     });
 }
-requirejs(['logManager'], function (LogManager) {
+requirejs(['common/LogManager'], function (LogManager) {
     LogManager.setLogLevel(0);
 });
 

commit 2d34fee7564d380ee41ec5a2a331ee801f433170

Fix gulp build-plugins, broken since 5f59a8bbc74c6aa45f

utils/build/webcyphy.plugins/cbuild.js
@@ -12,7 +12,7 @@
         jszip: './node_modules/webgme/src/client/lib/jszip/jszip',
         ejs: './node_modules/webgme/src/common/util/ejs',
         executor: './node_modules/webgme/src/middleware/executor',
-        superagent: './node_modules/webgme/node_modules/superagent/superagent',
+        superagent: './node_modules/webgme/src/client/lib/superagent/superagent',
         'plugin/AdmExporter': './src/plugins/ADMEditor',
         'plugin/TestBenchRunner': './src/plugins/ADMEditor',
         'plugin/AtmExporter': './src/plugins/ADMEditor',