13109d4a321a4be4410f97091c0f3dd7e347ea28
Zsolt Lattmann <zsolt.lattmann@gmail.com>
2015-07-01 16:04:09 -0500

    WEB-241 saving dummy data into the database then query it.

--- src/rest/testBenches/TestBenches.js
+++ src/rest/testBenches/TestBenches.js
@@ -7,7 +7,10 @@
 
 var ensureAuthenticated,
     gmeConfig, //global config is passed by server/standalone.js
-    logger;
+    logger,
+    mongoose = require('mongoose'),
+    Schema = mongoose.Schema,
+    ObjectId = Schema.ObjectId;
 
 
 // see webgme/src/server/api/index.js
@@ -22,15 +25,27 @@ function getUserId(req) {
 //    ensureAuthenticated(req, res, handleRequest);
 //};
 
+var ResultSchema = new Schema({
+    id: ObjectId,
+    userId: String,
+    testBenchId: String,
+    status: {type: String, match: /Running|Failed|Succeeded/, default: 'Running'},
+    startTime: String, // or Date ???
+    endTime: String, // or Date ???
+    resultUrl: String,
+    config: Object
+});
 
-var TestBenchesRestComponent = function (req, res, next) {
+// FIXME: name this collection as you wish
+var Result = mongoose.model('_test_bench_results', ResultSchema);
+
+var TestBenchesRestComponent = function (req, res/*, next*/) {
     var userId = getUserId(req),
 
-        // call next if request is not handled here.
+    // call next if request is not handled here.
         url = req.url.split('/'),
         testBenchId,
-        responseData,
-        i;
+        responseData;
 
     if (url.length === 2) {
         testBenchId = url[1];
@@ -41,34 +56,23 @@ var TestBenchesRestComponent = function (req, res, next) {
             results: []
         };
 
-        // TODO: read results from mongo
-        var status = ['Running', 'Failed', 'Succeeded'];
+        // read test bench results from mongo based on testBenchId
+        // Note: testBenchId might be an empty string
 
-        for (i = 0; i < 100; i += 1) {
-            responseData.results.push({
-                id: i,
-                testBenchId: testBenchId,
-                config: [
-                    {
-                        id: 1,
-                        name: 'quantity',
-                        value: 600
-                    }
-                ],
-                startTime: new Date((new Date()).getTime() - 20000 - Math.floor(Math.random() * 20000)).toISOString(),
-                endTime: new Date((new Date()).getTime() - Math.floor(Math.random() * 15000)).toISOString(),
-                status: status[Math.floor(Math.random() * status.length)],
-                resultUrl: 'something_' + i + '.zip'
+        Result.find({testBenchId: testBenchId}, function (err, docs) {
+            if (err) {
+                logger.error('error', err);
+                res.send(500);
+                return;
+            }
+
+            docs.forEach(function (doc) {
+                responseData.results.push(doc);
             });
 
-            if (responseData.results[i].status === 'Running') {
-                responseData.results[i].endTime = null;
-            }
-        }
-
-
-        // send results for test bench
-        res.json(responseData);
+            // send results for test bench
+            res.json(responseData);
+        });
 
     } else {
         res.sendStatus(404);
@@ -78,7 +82,46 @@ var TestBenchesRestComponent = function (req, res, next) {
 var setup = function (_gmeConfig, _ensureAuthenticated, _logger) {
     gmeConfig = _gmeConfig;
     logger = _logger.fork('gme:meta-morph:REST-TestBenches', true);
-    logger.info('got initialized');
+
+    // https://www.npmjs.com/package/mongoose#connecting-to-mongodb
+    // Important! Mongoose buffers all the commands until it's connected to the database.
+    // This means that you don't have to wait until it connects to MongoDB in order to define models, run queries, etc.
+    mongoose.connect(gmeConfig.mongo.uri);
+
+
+    // populate database with dummy data
+    var status = ['Running', 'Failed', 'Succeeded'],
+        i;
+
+    for (i = 0; i < 100; i += 1) {
+        var result = new Result();
+        result.testBenchId = i % 10;
+        result.config = [
+            {
+                id: 1,
+                name: 'quantity',
+                value: 600
+            }
+        ];
+        result.startTime = new Date((new Date()).getTime() - 20000 - Math.floor(Math.random() * 20000)).toISOString();
+        result.endTime = new Date((new Date()).getTime() - Math.floor(Math.random() * 15000)).toISOString();
+        result.resultUrl = 'something_' + i + '.zip';
+        result.status = status[Math.floor(Math.random() * status.length)];
+
+        if (result.status === 'Running') {
+            // clear end time if it is still running
+            result.endTime = null;
+        }
+
+        result.save(function (err) {
+            if (err) {
+                logger.error('Failed to save dummy test bench result: ', err);
+            }
+            logger.info('Added dummy test bench result.');
+        });
+    }
+
+
     ensureAuthenticated = _ensureAuthenticated;
     return TestBenchesRestComponent;
 };
