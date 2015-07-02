5c029194d5191570c793e0008ba477de38a5a6e3
Laszlo Juracz <laszlo.juracz@gmail.com>
2015-07-02 11:05:09 -0500

    custom config directives injected now

--- src/app/mmsApp/directives/testBenchDrawerPanel/testBenchConfig.js
+++ src/app/mmsApp/directives/testBenchDrawerPanel/testBenchConfig.js
@@ -3,7 +3,7 @@
 angular.module('mms.testBenchDrawerPanel.testBenchConfig', [
 ])
 
-.directive('testBenchConfig', function() {
+.directive('testBenchConfig', function($compile) {
 
     function TestBenchConfigController() {
 
@@ -15,20 +15,62 @@ angular.module('mms.testBenchDrawerPanel.testBenchConfig', [
         controllerAs: 'ctrl',
         bindToController: true,
         replace: true,
-        transclude: true,
+        transclude: false,
         scope: {
             testBench: '=',
             directive: '='
         },
+        require: ['testBenchConfig', '^testBenchDrawerPanelTestList'],
         template: '<div class="custom-test-bench-config-wrapper"><div class="place-holder"></div>',
-        link: function(scope, $el) {
+        link: function(scope, $el, attributes, controllers) {
 
-            var element = $el,
-                placeHolderEl = element.quesySelector('.place-holder');
+            var element = $el[0],
+                placeHolderEl = element.querySelector('.place-holder'),
+                ctrl = controllers[0],
+                listCtrl = controllers[1],
+                compiledDirective;
 
+            listCtrl.compiledConfigDirectives = listCtrl.compiledConfigDirectives || {};
 
+            if (ctrl.directive) {
 
+                compiledDirective = listCtrl.compiledConfigDirectives[ctrl.directive];
+
+                if (!compiledDirective) {
+
+                    compiledDirective = $compile(
+                        angular.element(
+                            '<' + ctrl.directive + '>' +
+                            '</' + ctrl.directive + '>'
+                        )
+                    );
+
+                    if (compiledDirective) {
+                        listCtrl.compiledConfigDirectives[ctrl.directive] = compiledDirective;
+                    } else {
+                        throw new Error('Config directive could not be compiled' + ctrl.directive);
+                    }
+                }
+
+                compiledDirective(scope, function(clonedElement) {
+                    element.replaceChild(clonedElement[0], placeHolderEl);
+                });
+
+            } else {
+                throw new Error('No config directive specified for testbench [' + ctrl.testBench.name + ']');
+            }
 
         }
     };
+})
+
+.directive('dummyTestBenchConfig', function() {
+
+    return {
+            restrict: 'E',
+            replace: true,
+            scope: true,
+            template: '<div>Dummy thing. Ain\'t anything to config with me. Upgrade to get the real thing.</div>'
+    };
+
 });
