31870fff780201d4a62d4a9ec2e326d0adc4af97
Kevin Smyth <kevin.m.smyth@gmail.com>
2015-07-21 16:03:07 -0500

    Use components.metamorphsoftware.com

--- deploy_prep.sh
+++ deploy_prep.sh
@@ -5,7 +5,7 @@ set -e
 [ -e node_modules ] && which cmd >/dev/null 2>&1 && cmd '/c rd /s/q node_modules'
 rm -rf node_modules
 [ -f npm-shrinkwrap.json ] && rm npm-shrinkwrap.json
-<src/app/mmsApp/config.client.default.json sed s@http://localhost:3000@${CONTENT_SERVER:-http://mmsapp-components.metamorphsoftware.com}@ > src/app/mmsApp/config.client.json
+<src/app/mmsApp/config.client.default.json sed s@http://localhost:3000@${CONTENT_SERVER:-http://components.metamorphsoftware.com}@ > src/app/mmsApp/config.client.json
 
 npm install --production
 npm shrinkwrap
