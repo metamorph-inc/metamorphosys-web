d4dc37a1305da59e838ad94ea2a5e770576739f4
Kevin Smyth <kevin.m.smyth@gmail.com>
2015-10-28 09:46:31 -0500

    CopyProject: dont need mongo. Cleanup

--- src/rest/copyproject/CopyProject.js
+++ src/rest/copyproject/CopyProject.js
@@ -1,4 +1,4 @@
-/* global define,require,console */
+/* global require,console */
 /*
  config.json: "rextrast": { "copyproject": "./src/rest/copyproject/CopyProject", ...
  http://localhost:8855/rest/external/copyproject
@@ -15,7 +15,6 @@ var path = require('path'),
     express = require('express'),
     router = express.Router(),
     BSON_FILE = path.join('dump', 'CyPhy', 'Template_Module_1x2.bson'),
-    mongodb = require('mongodb'),
     BSONStream = require('bson-stream');
 
 module.exports = {
@@ -29,63 +28,36 @@ function getUserId(req) {
 
 function copy(req, res, callback) {
     var projectName = 'Test_' + Math.floor(Math.random() * 100000);
-    var fatal = function (err) {
-        console.log(err);
-        callback(err);
-    };
-    mongodb.MongoClient.connect(gmeConfig.mongo.uri, {
-        db: {
-            'w': 1,
-            'native_parser': true
-        },
-        server: {
-            'auto_reconnect': true,
-            'poolSize': 20,
-            socketOptions: {
-                keepAlive: 1
-            }
+    var rs = fs.createReadStream(BSON_FILE);
+    var bsons = rs.pipe(new BSONStream());
+    var master;
+    bsons.on('data', function (obj) {
+        if (obj._id === '*master') {
+            master = obj.hash;
         }
-    }, function (err, db) {
+    });
+    bsons.on('end', function (err) {
+        if (!master) {
+            err = 'Couldnt find *master in bson';
+        }
         if (err) {
-            return fatal(err);
+            callback(err);
         }
-        fatal = function (err) {
-            db.close();
-            if (err) {
-                callback(err);
-            }
+        var parameters = {
+            userId: 'guest',
+            webGMESessionId: req.session.id,
+            type: 'db',
+            projectName: projectName,
+            seedName: 'Template_Module_1x2',
+            seedBranch: 'master',
+            seedCommit: master, // "#a79d96bbae12d1e44809f396dd38cd6bea6be272",
+            command: 'seedProject'
         };
-        var rs = fs.createReadStream(BSON_FILE);
-        var bsons = rs.pipe(new BSONStream());
-        var master;
-        bsons.on('data', function (obj) {
-            if (obj._id === '*master') {
-                master = obj.hash;
-            }
-        });
-        bsons.on('end', function (err) {
-            if (!master) {
-                err = 'Couldnt find *master in bson';
-            }
+        workerManager.request(parameters, function (err, result) {
             if (err) {
-                callback(err);
+                return callback(err);
             }
-            var parameters = {
-                userId: 'guest',
-                webGMESessionId: req.session.id,
-                type: 'db',
-                projectName: projectName,
-                seedName: 'Template_Module_1x2',
-                seedBranch: 'master',
-                seedCommit: master, // "#a79d96bbae12d1e44809f396dd38cd6bea6be272",
-                command: 'seedProject'
-            };
-            workerManager.request(parameters, function (err, result) {
-                if (err) {
-                    return callback(err);
-                }
-                callback(err, parameters.userId + '+' + projectName);
-            });
+            callback(err, parameters.userId + '+' + projectName);
         });
     });
 }
